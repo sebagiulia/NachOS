2d896ccb93e56195321744c813329e31f8f12a91
Milagros Osimi <milagrososimi@gmail.com>
2024-05-19 23:29:51 -0300

    cambio nombres

--- nachos/userland/rm.c
+++ nachos/userland/rm.c
@@ -1,7 +1,7 @@
 #include "syscall.h"
 
 #define ARGC_ERROR    "Error: missing argument."
-#define CREATE_ERROR  "Error: could not remove file."
+#define REMOVE_ERROR  "Error: could not remove file."
 
 int
 main(int argc, char *argv[])
@@ -14,7 +14,7 @@ main(int argc, char *argv[])
     int success = 1;
     for (unsigned i = 1; i < argc; i++) {
         if (Remove(argv[i]) < 0) {
-            Write(CREATE_ERROR, sizeof(CREATE_ERROR) - 1, CONSOLE_OUTPUT);
+            Write(REMOVE_ERROR, sizeof(REMOVE_ERROR) - 1, CONSOLE_OUTPUT);
             success = 0;
         }
     }
