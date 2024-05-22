ea614cc3c905e57d97ed8f111ed77c36e522e133
NerinaRodriguez <emrcnyrc@gmail.com>
2024-05-21 22:49:24 -0300

    segundo plano tinyshell

--- nachos/userland/tinyshell.c
+++ nachos/userland/tinyshell.c
@@ -11,6 +11,8 @@ main(void)
     char       prompt[2] = { '-', '-' };
     char       ch, buffer[60];
     int        i;
+    char *line;
+    int runInBackground = 0;
 
     for (;;) {
         Write(prompt, 2, output);
@@ -21,9 +23,17 @@ main(void)
 
         buffer[--i] = '\0';
 
+        line = buffer;
+        if(buffer[0] == '&') {
+            line = &buffer[1];
+            runInBackground = 1;
+        }
+
         if (i > 0) {
-            newProc = Exec(buffer);
-            int e = Join(newProc);
+            newProc = Exec(line);
+            if(!runInBackground)
+                Join(newProc);
+            runInBackground = 0;
         }
     }
 
